06e9708aaa4dcf771c55159436989baa298bd801
Tiago Citrangulo da Silva <130724423+TiagoCitranguloDaSilva@users.noreply.github.com>
2023-07-30 14:46:25 -0300

    Adicionando movimento

--- code.js
+++ code.js
@@ -1,6 +1,11 @@
 
-var mesa = document.getElementById('container')
-var qtdeColunas = 6
+var origem;
+var absoluteLocal = 40;
+var elemento;
+var local;
+var blur = document.getElementById('blur');
+var mesa = document.getElementById('container');
+var qtdeColunas = 6;
 var CartasPossiveis = [];
 var Naipes = ['espada', 'copas', 'paus', 'ouros'];
 for(let c = 0; c < Naipes.length; c++){
@@ -35,21 +40,21 @@ function iniciarJogo(){
         // Criar coluna
 
         let coluna = document.createElement('section');
-        coluna.id = 'coluna'+c;
+        coluna.id = 'coluna'+(c+1);
         coluna.classList.add('coluna')
         mesa.appendChild(coluna);
-        let colunaAtual = document.getElementById('coluna'+c);
+        let colunaAtual = document.getElementById('coluna'+(c+1));
         let escolhido;
-        let classes = [];
         for(let d = 0; d <= c; d++){
+            let classes = [];
             escolhido = aleatorizar();
             if(d != c){
                 classes.push('escondida');
             }
-            let cartaAtual = criarCarta(escolhido, classes);
+            let cartaAtual = criarCarta(escolhido, classes, true);
             let contagem = 0    
             for(let e = 0; e < colunaAtual.children.length; e++){
-                contagem += 60; 
+                contagem += absoluteLocal; 
             }
             cartaAtual.style.top = contagem+'px';
             colunaAtual.appendChild(cartaAtual);
@@ -61,22 +66,91 @@ function aleatorizar(){
     return Math.floor(Math.random() * (CartasPossiveis.length - 1)) + 1;
 }
 
-function criarCarta(escolha, classes, removerCartasPossiveis = false){
+function criarCarta(escolha, classes, removerCartasPossiveis = false, aleatorizar = true){
     
-    let escolhida = CartasPossiveis[escolha];
+    let escolhida;
+    if(aleatorizar){
+        escolhida = CartasPossiveis[escolha];
+    }else{
+        escolhida = escolha;
+    }
     let carta = document.createElement('section');
     carta.classList.add('cartas');
     for(let c = 0; c < classes.length; c++){
         carta.classList.add(classes[c]);
     }
-    console.log(escolhida)
     carta.classList.add(escolhida[0]);
-
+    carta.setAttribute('onclick', 'mover(this)');
     carta.innerHTML = escolhida[1];
+    carta.id = escolhida[0]+'-'+escolhida[1];
 
     if(removerCartasPossiveis){
-        CartasPossiveis.splice(escolha);
+        CartasPossiveis.splice(escolha, 1);
     }
 
     return carta;
+}
+
+function mover(element){
+    blur.style.display = 'flex';
+    elemento = element;
+}
+
+function escolha(valorEscolha){
+    blur.style.display = 'none';
+    switch(valorEscolha){
+        case '1':
+            local = 'coluna1';
+            break;
+        case '2':
+            local = 'coluna2';
+            break;
+        case '3':
+            local = 'coluna3';
+            break;
+        case '4':
+            local = 'coluna4';
+            break;
+        case '5':
+            local = 'coluna5';
+            break;
+        case '6':
+            local = 'coluna6';
+            break;
+        case '7':
+            local = 'AreaFinal';
+            break;
+    }
+
+    verificacoes()
+}
+
+function verificacoes(){
+    finalizar()
+}
+
+function finalizar(){
+    origem = elemento.parentElement.id;
+    let localAdd;
+    if(local == 'AreaFinal'){
+        alert('n fiz ainda');
+    }else{
+        localAdd = document.getElementById(local);
+    }
+    elemento.remove();
+    conteudo = [elemento.classList[1], elemento.innerHTML];
+    let cartaAtual = criarCarta(conteudo, [], false, false);
+    let altura = (localAdd.children.length) * absoluteLocal;
+    cartaAtual.style.top = altura+'px';
+    localAdd.appendChild(cartaAtual);
+    
+    aparecer()
+}
+
+function aparecer(){
+    if(document.getElementById(origem).children.length > 0){
+        if(document.getElementById(origem).children[document.getElementById(origem).children.length - 1].classList[1] == 'escondida'){
+            document.getElementById(origem).children[document.getElementById(origem).children.length - 1].classList.remove('escondida');
+        }
+    }
 }
